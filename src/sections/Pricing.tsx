494cea1c82c8ab03cafc2d73d3350e91c35285f5
Nicholas Yeung <njyeung@wisc.edu>
2025-02-09 22:29:11 -0600

    asjkhdajksd

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,5 @@
+import CheckIcon from '@/assets/check.svg'
+import { twMerge } from 'tailwind-merge';
 const pricingTiers = [
   {
     title: "Free",
@@ -51,5 +53,36 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return <section className="py-24">
+    <div className="container">
+      <h2 className='section-header header-gradient section-heading-wrapper'>Pricing</h2>
+      <p className="section-description mt-5">Free forever. Upgrade for unlimited tasks, better security, and exclusive features</p>
+      <div className='flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end justify-center'>
+        {pricingTiers.map((tier, index)=>
+          <div className={twMerge('p-10 border border-[#e1e1e1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full', index%2==1 ? 'border-black bg-black text-white' : '')}>
+            <div className='flex justify-between'>
+              <h3 className={twMerge('text-lg font-bold text-black/50', index%2==1 ? 'text-white/60' : '' )}>{tier.title}</h3>
+              { tier.popular === true ? 
+              <div className='inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20'>
+                <span className='popular-gradient text-transparent bg-clip-text font-medium'>Popular</span>
+              </div> : <></> }
+            </div>
+            <div className='flex items-baseline gap-1 mt-[30px]'>
+              <span className='text-4xl font-bold tracking-tighter leading-none'>{tier.monthlyPrice}</span>
+              <span className={twMerge('tracking-tight font-bold text-black/50', index%2==1 ? 'text-white/60' : '')}>/month</span>
+            </div>
+            <button className={twMerge('btn btn-primary w-full mt-[30px]', index%2==1 ? 'bg-white text-black' : '')} >{tier.buttonText}</button>
+            <ul className='flex flex-col gap-5 mt-8'>
+              {tier.features.map((feature)=>
+                <li className='text-sm flex items-center gap-4'>
+                  <CheckIcon className="h-6 w-6" />
+                  <span>{feature}</span>
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  </section>;
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -4,14 +4,14 @@ import pyramidImage from '@/assets/pyramid.png'
 import tubeImage from '@/assets/tube.png'
 
 export const ProductShowcase = () => {
-  return <section className="bg-gradient-to-b from-[#FFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+  return <section className="bg-gradient-to-b from-[#FFFF] to-[#D2DCFF] py-24 overflow-x-clip">
     <div className="container">
-      <div className="max-w-2xl mx-auto ">
+      <div className="section-heading-wrapper">
         <div className="flex justify-center">
           <div className="tag">Boost your productivity</div>
         </div>
-        <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter header-gradient mt-5">A more effective way to track prograss</h2>
-        <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">Effortlessly turn your ideas into a fully functional, responsive, SaaS website in just minutes with this template.</p>
+        <h2 className="section-header header-gradient mt-5">A more effective way to track prograss</h2>
+        <p className="section-description mt-5">Effortlessly turn your ideas into a fully functional, responsive, SaaS website in just minutes with this template.</p>
         
       </div>
       <div className="relative mt-10">
@@ -19,7 +19,6 @@ export const ProductShowcase = () => {
         <Image height={262} width={262} className='hidden md:block absolute -right-36 -top-32 ' src={pyramidImage} alt="pyramid image"></Image>
         <Image height={248} width={248} className='hidden md:block absolute bottom-24 -left-36' src={tubeImage} alt="pyramid image"></Image>
       </div>
-      
     </div>
   </section>;
 };
